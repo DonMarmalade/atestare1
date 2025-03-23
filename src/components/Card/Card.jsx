f72f3e3f8fcde22010b2f4d00be3298ac0f1032b
Don Victor <darkemeraldplay@gmail.com>
2025-03-23 11:54:19 +0200

    Second try (finală)

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,11 +2,13 @@ import styles from './Card.module.css';
 
 function Card({image, onClick}) {
     let CSSSelectedStyle = image.isSelected && styles.selected
+    let CSSPassedStyle = image.isPassed && styles.passed
+
 
     return (
         <>
             <img
-                className={`${styles.image} ${CSSSelectedStyle}`}
+                className={`${styles.image} ${CSSSelectedStyle} ${CSSPassedStyle}`}
                 key={image.key}
                 alt={image.title}
                 src={image.isSelected ? image.url : 'src/assets/question.svg'}
--- src/components/Library/Library.jsx
+++ src/components/Library/Library.jsx
@@ -3,108 +3,150 @@ import images from '../../data/images.json'
 import Card from '../Card/index.js'
 import {useState} from 'react'
 
-const imagesNewField = images.map(image => image = {...image, isSelected: false, isPassed: false})
+const newLibrary = images.map(image => image = {...image, isSelected: false, isPassed: false})
 
 function Library() {
-    const[libraryFirst, setLibraryFirst] = useState(imagesNewField)
-    const[librarySecond, setLibrarySecond] = useState(imagesNewField)
-    const[isSelectedFirst, setSelectedFirst] = useState(images[0])
-    const[isSelectedSecond, setSelectedSecond] = useState(images[1])
+    const[libraryFirst, setLibraryFirst] = useState(newLibrary.sort(() => Math.random() - 0.5))
+    const[librarySecond, setLibrarySecond] = useState(newLibrary.sort(() => Math.random() - 0.5))
+    const[isPressed, setIsPressed] = useState(null)
+    const[count, setCount] = useState(0)
 
-    function checkPass() {
-        if(isSelectedFirst.id === isSelectedSecond.id) {
-            console.log("1")
-            const newFirstImage = libraryFirst.map(image => {
-                if(isSelectedFirst.id === image.id || isSelectedSecond.id === image.id) {
-                    return {...image, isSelected: true, isPassed: true}
+    function timeOut(newImageFirst, newImageSecond, id) {
+        const[newImageFirstPassed, newImageSecondPassed] = isPressedVerify(newImageFirst, newImageSecond, id)
+        setTimeout(
+            () => {
+                const newImageFirstPassedSelected = newImageFirstPassed.map(image => {
+                    if (image.isPassed === true) {
+                        return {...image, isSelected: true}
+                    } else {
+                        return {...image, isSelected: false}
+                    }
+                })
+
+                const newImageSecondPassedSelected = newImageSecondPassed.map(image => {
+                    if (image.isPassed === true) {
+                        return {...image, isSelected: true}
+                    } else {
+                        return {...image, isSelected: false}
+                    }
+                })
+
+                setLibraryFirst(newImageFirstPassedSelected)
+                setLibrarySecond(newImageSecondPassedSelected)
+            }, 200
+        )
+    }
+
+    function isPressedVerify(newImageFirst, newImageSecond, id) {
+        let newImageFirstPassed = newImageFirst.map(image => {
+            if (image.isPassed === false) {
+                if (image.id === isPressed.id && image.id === id) {
+                    return {...image, isPassed: true}
                 } else {
-                    return {...image, isSelected: false, isPassed: false}
+                    return {...image, isPassed: false}
                 }
+            } else {
+                return {...image, isPassed: true}
+            }
             })
 
-            const newSecondImage = librarySecond.map(image => {
-                if(isSelectedSecond.id === image.id || isSelectedFirst.id === image.id) {
-                    return {...image, isSelected: true, isPassed: true}
+        let newImageSecondPassed = newImageSecond.map(image => {
+            if (image.isPassed === false) {
+                if (image.id === isPressed.id && image.id === id) {
+                    return {...image, isPassed: true}
                 } else {
-                    return {...image, isSelected: false, isPassed: false}
+                    return {...image, isPassed: false}
                 }
+            } else {
+                return {...image, isPassed: true}
+            }
             })
+        setLibraryFirst(newImageFirstPassed)
+        setLibrarySecond(newImageSecondPassed)
 
-            setLibraryFirst(newFirstImage)
-            setLibrarySecond(newSecondImage)
-        } else {
-            console.log("2")
-            const newFirstImage = libraryFirst.map(image => {
-                if(isSelectedFirst.id === image.id || isSelectedSecond.id === image.id) {
-                    return {...image, isSelected: true, isPassed: false}
-                } else {
-                    return {...image, isSelected: false, isPassed: false}
-                }
-            })
-
-            const newSecondImage = librarySecond.map(image => {
-                if(isSelectedSecond.id === image.id || isSelectedFirst.id === image.id) {
-                    return {...image, isSelected: true, isPassed: true}
-                } else {
-                    return {...image, isSelected: false, isPassed: false}
-                }
-            })
-
-            setLibraryFirst(newFirstImage)
-            setLibrarySecond(newSecondImage)
-        }
+        return [newImageFirstPassed, newImageSecondPassed]
     }
 
     function handleOnClickFirst(id) {
-        const newImages = libraryFirst.map(image => {
-            if(image.isPassed === false) {
-                if(image.id === id) {
-                    setSelectedFirst(image)
-                    return {...image, isSelected: true, isPassed: false}
+        let newImageFirst = libraryFirst.map(image => {
+            if (image.isPassed === false) {
+                if (image.id === id) {
+                    setIsPressed(image)
+                    return {...image, isSelected: true}
                 } else {
-                    return {...image, isSelected: false, isPassed: false}
+                    return {...image, isSelected: false}
                 }
             } else {
-                return {...image, isSelected: true, isPassed: true}
+                return {...image, isSelected: true}
             }
         })
 
-        setLibraryFirst(newImages)
-        checkPass()
+        let newImageSecond = [...librarySecond]
+
+        if(isPressed) {
+            setCount(c => c + 1)
+            timeOut(newImageFirst, newImageSecond, id)
+            setIsPressed(null)
+        } else {
+            setLibraryFirst(newImageFirst)
+        }
     }
 
     function handleOnClickSecond(id) {
-        const newImages = librarySecond.map(image => {
-            if(image.isPassed === false) {
-                if(image.id === id) {
-                    setSelectedSecond(image)
-                    return {...image, isSelected: true, isPassed: false}
+        let newImageSecond = librarySecond.map(image => {
+            if (image.isPassed === false) {
+                if (image.id === id) {
+                    setIsPressed(image)
+                    return {...image, isSelected: true}
                 } else {
-                    return {...image, isSelected: false, isPassed: false}
+                    return {...image, isSelected: false}
                 }
             } else {
-                return {...image, isSelected: true, isPassed: true}
+                return {...image, isSelected: true}
             }
         })
 
-        setLibrarySecond(newImages)
-        checkPass()
+        let newImageFirst = [...libraryFirst]
+
+        if(isPressed) {
+            setCount(c => c + 1)
+            timeOut(newImageFirst, newImageSecond, id)
+            setIsPressed(null)
+        } else {
+            setLibrarySecond(newImageSecond)
+        }
     }
 
+    function handleVictory() {
+        if (libraryFirst.find(image => image.isPassed === false) === undefined) {
+            return (
+                <div className={styles.victory}>
+                    <h1>You Won!</h1>
+                    <h1>You pressed: {count} times</h1>
+                </div>
+            )
+        } else {
+            return (
+                <div className={styles.library}>
+                    {
+                        libraryFirst.map(image =>
+                            <Card key={image.id} image={image} onClick={() => handleOnClickFirst(image.id)} />
+                        )
+                    }
+                    {
+                        librarySecond.map(image =>
+                            <Card key={image.id} image={image} onClick={() => handleOnClickSecond(image.id)} />
+                        )
+                    }
+                </div>
+            )
+        }
+    }
     return (
         <>
-            <div className={styles.library}>
-                {
-                    libraryFirst.map(image =>
-                        <Card key={image.id} image={image} onClick={() => handleOnClickFirst(image.id)} />
-                    )
-                }
-                {
-                    librarySecond.map(image =>
-                        <Card key={image.id} image={image} onClick={() => handleOnClickSecond(image.id)} />
-                    )
-                }
-            </div>
+            {
+                handleVictory()
+            }
         </>
     )
 }
